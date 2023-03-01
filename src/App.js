75e8051528b1aa5950b0d3b36464a0ece30a8d84
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-03-02 09:27:05 +1100

    added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { SearchForm } from './components/SearchForm';
+import { Display } from './components/Display';
+import { CustomCard } from './components/CustomCard'
+import { Container, Button, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Row>
+          <Col>
+            asdads</Col>
+        </Row>
+      </Container>
+
 
     </div>
   );
